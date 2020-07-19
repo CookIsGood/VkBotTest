3fc84bb95bedde402b9c467dfc09b084a1a41479
CookIsGood <Morkovo4ka513@yandex.ru>
2020-07-19 03:43:22 +0300

    Init commit

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def send_message(vk_session, id_type, id, message=None):
                       {id_type: id, 'message': message, 'random_id': random.randint(-2147483648, +2147483648)})
 
 
-def get_members(vk_session, group_id="197230708", count=1000):
+def get_members(vk_session, group_id="#", count=1000):
     vk_session.method('groups.getMembers',
                       {'group_id': group_id, 'count': count})
 
@@ -38,13 +38,13 @@ def main():
 
     monitor_msg(vk_session)
 
-    # server1 = Server(vk_api_token, 197230708, "server1")
+    # server1 = Server(vk_api_token, #, "server1")
     # vk_api_token - API токен, который мы ранее создали
     # 172998024 - id сообщества-бота
     # "server1" - имя сервера
 
     # server1.test()
-    # send_message(vk_session, "user_id", 114220893, message="Привет")
+    # send_message(vk_session, "user_id", #, message="Привет")
 
 
 if __name__ == '__main__':
