d0e638216658e23f263de0eeddb11ca24ba163a1
CookIsGood <65176958+CookIsGood@users.noreply.github.com>
2020-07-31 00:10:47 +0300

    added control admin

--- keyboard.py
+++ keyboard.py
@@ -12,35 +12,35 @@ def create_keyboard(response, values):
     if response == 'начать' or response == 'вернуться назад':
 
         keyboard.add_button('О нас', color=VkKeyboardColor.POSITIVE)
-        keyboard.add_button('Узнать свой id', color=VkKeyboardColor.POSITIVE)
-        keyboard.add_line()
-        keyboard.add_button('Команда 3', color=VkKeyboardColor.POSITIVE)
-        keyboard.add_button('Команда 4', color=VkKeyboardColor.POSITIVE)
-        if values == 1:
+        keyboard.add_button('Узнать id', color=VkKeyboardColor.POSITIVE)
+        if values == 1 or values == 2:
             keyboard.add_line()  # Переход на вторую строку
             keyboard.add_button('Рассылка', color=VkKeyboardColor.NEGATIVE)
+            keyboard.add_button('РассылкаОпрос', color=VkKeyboardColor.NEGATIVE)
+            keyboard.add_line()
             keyboard.add_button('Управление админами', color=VkKeyboardColor.NEGATIVE)
-        keyboard.add_line()
-        keyboard.add_button('Вернуться назад', color=VkKeyboardColor.PRIMARY)
-        keyboard.add_line()
-        keyboard.add_button('Закрыть клавиатуру', color=VkKeyboardColor.NEGATIVE)
-    if (response == 'рассылка') and (values == 1):
+    if (response == 'рассылка') and (values == 1 or values == 2):
         keyboard.add_button('Создать рассылку', color=VkKeyboardColor.POSITIVE)
         keyboard.add_line()
         keyboard.add_button('Отправить рассылку', color=VkKeyboardColor.NEGATIVE)
         keyboard.add_line()
         keyboard.add_button('Вернуться назад', color=VkKeyboardColor.PRIMARY)
-        keyboard.add_line()
-        keyboard.add_button('Закрыть клавиатуру', color=VkKeyboardColor.NEGATIVE)
-    if (response == 'управление админами') and (values == 1):
+    if (response == 'управление админами') and (values == 1 or values == 2):
         keyboard.add_button('Список админов', color=VkKeyboardColor.POSITIVE)
-        keyboard.add_line()
-        keyboard.add_button('Добавить админа', color=VkKeyboardColor.NEGATIVE)
-        keyboard.add_button('Удалить админа', color=VkKeyboardColor.NEGATIVE)
+        if values == 2:
+            keyboard.add_line()
+            keyboard.add_button('Добавить админа', color=VkKeyboardColor.NEGATIVE)
+            keyboard.add_button('Удалить админа', color=VkKeyboardColor.NEGATIVE)
         keyboard.add_line()
         keyboard.add_button('Вернуться назад', color=VkKeyboardColor.PRIMARY)
+    if (response == 'рассылкаопрос') and (values == 1 or values == 2):
+        keyboard.add_button('Создать сообщение', color=VkKeyboardColor.POSITIVE)
+        keyboard.add_button('Сохранить сообщение', color=VkKeyboardColor.NEGATIVE)
         keyboard.add_line()
-        keyboard.add_button('Закрыть клавиатуру', color=VkKeyboardColor.NEGATIVE)
+        keyboard.add_button('Создать id опроса', color=VkKeyboardColor.POSITIVE)
+        keyboard.add_button('Сохранить id опроса', color=VkKeyboardColor.NEGATIVE)
+        keyboard.add_line()
+        keyboard.add_button('Вернуться назад', color=VkKeyboardColor.PRIMARY)
 
     elif response == 'закрыть клавиатуру':
         print('Закрываем клавиатуру')
--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from vk_api import VkUpload
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
 # Приватные файлы
-from login import vk_api_token, vk_id_group, vk_token_server
-import video_get
+from login import vk_api_token, vk_id_group, my_id, vk_id_group_neg
 # Вспомогательные библиотеки
 import sqlite3
 import threading
@@ -34,6 +33,7 @@ def monitor_msg(vk_session, session_api):
             # Присоеденение к БД
             # 0 - участник группы
             # 1 - админ группы
+            # 2 - админ админов группы
             if event.from_user and not event.from_me:
                 db = sqlite3.connect('server.db')
                 sql = db.cursor()
@@ -44,9 +44,11 @@ def monitor_msg(vk_session, session_api):
                 else:
                     values = 1
                 db.close()
+                if event.user_id == my_id:
+                    values = 2
                 new_keyboard = keyboard.create_keyboard(response, values)
                 # Если нажимают: "1"
-                if (response == 'рассылка') and (values == 1):
+                if (response == 'рассылка') and (values == 1 or values == 2):
                     send_message(vk_session, id_type='user_id', id_user=event.user_id,
                                  message="Вы попали в меню рассылки!",
                                  keyboard=new_keyboard, attachment=None)
@@ -67,19 +69,11 @@ def monitor_msg(vk_session, session_api):
                     attach_list = ','.join(buff)
                     send_message(vk_session, id_type='user_id', id_user=event.user_id, message="Ссылка на видео",
                                  keyboard=None, attachment=attach_list)
-                elif response == 'команда 3':
-                    print("Ввели команду: команда 3")
-                    send_message(vk_session, id_type='user_id', id_user=event.user_id, message="Вы ввели команду 3",
-                                 keyboard=None, attachment=None)
-                elif response == 'команда 4':
-                    print("Ввели команду: команда 4")
-                    send_message(vk_session, id_type='user_id', id_user=event.user_id, message="Вы ввели команду 4",
-                                 keyboard=None, attachment=None)
                 elif response == 'закрыть клавиатуру':
                     print("Ввели команду: закрыть клавиатуру")
                     send_message(vk_session, id_type='user_id', id_user=event.user_id, message="Вы закрыли клавиатуру",
                                  keyboard=new_keyboard, attachment=None)
-                elif (response == 'создать рассылку') and (values == 1):
+                elif (response == 'создать рассылку') and (values == 1 or values == 2):
                     with open("Инструкция.txt", 'r', encoding="utf-8") as f:
                         message_main = f.read()
                     print("Ввели команду: создать рассылку")
@@ -89,7 +83,7 @@ def monitor_msg(vk_session, session_api):
                     attach_send_in = open('Вложение рассылки.txt', 'w', encoding='utf-8')
                     text_send_in.truncate()
                     attach_send_in.truncate()
-                elif (response == 'отправить рассылку') and (values == 1):
+                elif (response == 'отправить рассылку') and (values == 1 or values == 2):
                     text_final = open("Текст рассылки.txt", 'r', encoding="utf-8")
                     attach_final = open("Вложение рассылки.txt", 'r', encoding="utf-8")
                     send_text_final = text_final.read()
@@ -119,7 +113,7 @@ def monitor_msg(vk_session, session_api):
                             i += 1
                             print("Пользователю не отправилось сообщение!")
                             continue
-                elif (response == 'управление админами') and (values == 1):
+                elif (response == 'управление админами') and (values == 1 or values == 2):
                     print("Ввели команду: управление админами")
                     send_message(vk_session, id_type='user_id', id_user=event.user_id,
                                  message="Вы попали в меню управление админами группы! \nДля того, чтобы "
@@ -127,16 +121,16 @@ def monitor_msg(vk_session, session_api):
                                  keyboard=new_keyboard, attachment=None)
                     text_final = open("Текст рассылки.txt", 'w', encoding="utf-8")
                     text_final.truncate()
-                elif (response == 'список админов') and (values == 1):
+                elif (response == 'список админов') and (values == 1 or values == 2):
                     print("Ввели команду: список админов")
                     db = sqlite3.connect('server.db')
                     sql = db.cursor()
                     dataCount = sql.execute(
-                        f"SELECT COUNT(idlogin)  FROM users WHERE permession = '{1}'")
+                        f"SELECT COUNT(idlogin)  FROM users WHERE permession = '{1}' OR permession = '{2}'")
                     numberOfRows = dataCount.fetchone()[0]
                     # reg = re.compile('[^a-zA-Z ]')
                     dataCopy = sql.execute(
-                        f"SELECT idlogin  FROM users WHERE permession = '{1}'")
+                        f"SELECT idlogin  FROM users WHERE permession = '{1}' OR permession = '{2}'")
                     values = dataCopy.fetchmany(size=numberOfRows)
                     print("Кол во:" + str(numberOfRows))
                     print(type(values))
@@ -146,7 +140,7 @@ def monitor_msg(vk_session, session_api):
                                      keyboard=None, attachment=None)
                     text_final = open("Текст рассылки.txt", 'w', encoding="utf-8")
                     text_final.truncate()
-                elif (response == 'добавить админа') and (values == 1):
+                elif (response == 'добавить админа') and (values == 2):
                     print("Ввели команду: добавить админа")
                     text_final = open("Текст рассылки.txt", 'r', encoding="utf-8")
                     send_text_final = text_final.read()
@@ -161,7 +155,7 @@ def monitor_msg(vk_session, session_api):
                                  keyboard=None, attachment=None)
                     text_final = open("Текст рассылки.txt", 'w', encoding="utf-8")
                     text_final.truncate()
-                elif (response == 'удалить админа') and (values == 1):
+                elif (response == 'удалить админа') and (values == 2):
                     print("Ввели команду: удалить админа")
                     text_final = open("Текст рассылки.txt", 'r', encoding="utf-8")
                     send_text_final = text_final.read()
@@ -176,39 +170,86 @@ def monitor_msg(vk_session, session_api):
                                  keyboard=None, attachment=None)
                     text_final = open("Текст рассылки.txt", 'w', encoding="utf-8")
                     text_final.truncate()
-                elif response == 'узнать свой id':
+                elif response == 'узнать id':
                     print("Ввели команду: узнать свой id")
                     send_message(vk_session, id_type='user_id', id_user=event.user_id,
                                  message=f"Ваш id: {event.user_id}",
                                  keyboard=None, attachment=None)
+                elif (response == 'рассылкаопрос') and (values == 1 or values == 2):
+                    print("Ввели команду: рассылкаопрос")
+                    send_message(vk_session, id_type='user_id', id_user=event.user_id,
+                                 message=f"рассылкаопрос",
+                                 keyboard=new_keyboard, attachment=None)
+                elif (response == 'создать сообщение') and (values == 1 or values == 2):
+                    print("Ввели команду: создать сообщение")
+                    send_message(vk_session, id_type='user_id', id_user=event.user_id,
+                                 message=f"Введите текст рассылки для проголосовавших в опросе и можете прикрепить "
+                                         f"вложения...",
+                                 keyboard=None, attachment=None)
+                elif (response == 'сохранить сообщение') and (values == 1 or values == 2):
+                    print("Ввели команду: сохранить сообщение")
+                    send_message(vk_session, id_type='user_id', id_user=event.user_id,
+                                 message=f"Текст рассылки для опроса успешно сохранен!",
+                                 keyboard=None, attachment=None)
+                elif (response == 'создать id опроса') and (values == 1 or values == 2):
+                    print("Ввели команду: создать id опроса")
+                    send_message(vk_session, id_type='user_id', id_user=event.user_id,
+                                 message=f"Введите id опроса в формате: 111111111",
+                                 keyboard=None, attachment=None)
+                elif (response == 'сохранить id опроса') and (values == 1 or values == 2):
+                    print("Ввели команду: сохранить id опроса")
+                    send_message(vk_session, id_type='user_id', id_user=event.user_id,
+                                 message=f"Id опроса успешно сохранен!",
+                                 keyboard=None, attachment=None)
+                elif values == 1 or values == 2:
+                    msg_history = vk_session.method('messages.getHistory',
+                                                    {'offset': 2, 'count': 1, 'user_id': event.user_id,
+                                                     'group_id': vk_id_group})
+                    msg_text = msg_history["items"][0]["text"]
+                    print("ОБРАТИ НА МЕНЯ ВНИМАНИЕ: " + str(msg_text))
+                    if msg_text == 'создать рассылку' or msg_text == 'Создать рассылку':
+                        buff_type = []
+                        buff_id = []
+                        for i in range(1, 10):
+                            try:
+                                id_attach = event.attachments["attach" + str(i)]
+                                buff_id.append(id_attach)
+                                type_attach = event.attachments["attach" + str(i) + "_type"]
+                                buff_type.append(type_attach)
+                            except KeyError:
+                                pass
+                        buff = []
+                        for i in range(0, len(buff_type)):
+                            buff.append(str(buff_type[i]) + str(buff_id[i]))
+                        final_buff = []
+                        for items in buff:
+                            final_buff.append(items)
+                        attach_list = ','.join(final_buff)
+                        text_final = open("Текст рассылки.txt", 'r+', encoding="utf-8")
+                        attach_final = open("Вложение рассылки.txt", 'r+', encoding="utf-8")
+                        new_text_send = text_final.read()
+                        new_attach_send = attach_final.read()
+                        new_text_send = re.sub(new_text_send, str(event.text), new_text_send)
+                        new_attach_send = re.sub(new_attach_send, str(attach_list), new_attach_send)
+                        text_final.write(new_text_send)
+                        attach_final.write(new_attach_send)
+                        text_final.close()
+                        attach_final.close()
+                    elif msg_text == 'создать сообщение' or msg_text == 'Создать сообщение':
+                        pass
+                    elif msg_text == 'cоздать id опроса' or msg_text == 'Создать id опроса':
+                        pass
+                    else:
+                        print("Неопозннная команда...")
+                        send_message(vk_session, id_type='user_id', id_user=event.user_id,
+                                     message=f"Я тебя не понимаю. \nДля того, чтобы посмотреть мои команды введи: начать",
+                                     keyboard=None, attachment=None)
+
                 else:
-                    buff_type = []
-                    buff_id = []
-                    for i in range(1, 10):
-                        try:
-                            id_attach = event.attachments["attach" + str(i)]
-                            buff_id.append(id_attach)
-                            type_attach = event.attachments["attach" + str(i) + "_type"]
-                            buff_type.append(type_attach)
-                        except KeyError:
-                            pass
-                    buff = []
-                    for i in range(0, len(buff_type)):
-                        buff.append(str(buff_type[i]) + str(buff_id[i]))
-                    final_buff = []
-                    for items in buff:
-                        final_buff.append(items)
-                    attach_list = ','.join(final_buff)
-                    text_final = open("Текст рассылки.txt", 'r+', encoding="utf-8")
-                    attach_final = open("Вложение рассылки.txt", 'r+', encoding="utf-8")
-                    new_text_send = text_final.read()
-                    new_attach_send = attach_final.read()
-                    new_text_send = re.sub(new_text_send, str(event.text), new_text_send)
-                    new_attach_send = re.sub(new_attach_send, str(attach_list), new_attach_send)
-                    text_final.write(new_text_send)
-                    attach_final.write(new_attach_send)
-                    text_final.close()
-                    attach_final.close()
+                    print("Неопозннная команда...")
+                    send_message(vk_session, id_type='user_id', id_user=event.user_id,
+                                 message=f"Я тебя не понимаю. \nДля того, чтобы посмотреть мои команды введи: начать",
+                                 keyboard=None, attachment=None)
 
 
 # Функция отвечающая за отправку сообщений участникам
@@ -220,10 +261,29 @@ def send_message(vk_session, id_type, id_user, message=None, keyboard=None, atta
 
 # Исполняющая программу функция
 def main():
-    print("Приложение запущено")
+    db = sqlite3.connect('server.db')
+    sql = db.cursor()
+    sql.execute("""CREATE TABLE IF NOT EXISTS users (
+              idlogin TEXT,
+              permession INT,
+              UNIQUE(idlogin, permession)
+              )""")
+    sql.execute(f"INSERT OR IGNORE INTO users VALUES ('{my_id}', '{2}')")
+    db.commit()
     # vk-api (longpool)
     vk_session = vk_api.VkApi(token=vk_api_token)
     session_api = vk_session.get_api()
+
+    members = vk_session.method('groups.getMembers', {'group_id': vk_id_group, 'count': 1000})
+    # Присваиваем id участников в отдельную переменную
+    users_items = members["items"]
+    # Получаем количество участников группы
+    count_users_items = members["count"]
+    for i in range(count_users_items):
+        sql.execute(f"INSERT OR IGNORE INTO users VALUES ('{users_items[i]}', '{0}')")
+        db.commit()
+    db.close()
+    print("Приложение запущено")
     monitor_msg(vk_session, session_api)
 
 
